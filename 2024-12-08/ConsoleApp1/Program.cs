01db06e23502bd4b237780da1069e0fe8a9dfa6f
陈序源 <chenjx0395@163.com>
2024-12-08 10:59:45 +0800

    add:反转单词

--- 2024-12-08/ConsoleApp1/Program.cs
+++ 2024-12-08/ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+string[] arr01 = pangram.Split(" ");
+//Array.Reverse(arr01);
+string[] arr02 = new string[arr01.Length];
+for (int i = 0; i < arr01.Length; i++)
+{
+    char[] temp = arr01[i].ToCharArray();
+    Array.Reverse(temp);
+    arr02[i] = new string(temp);
+}
+pangram =  String.Join(" ",arr02);
+Console.WriteLine(pangram);
--- 2024-12-08/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 2024-12-08/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae8015e56cda47de63257e8137c7a6b873417b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
