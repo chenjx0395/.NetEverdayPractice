c35993047c706c4182cdad25de1fbb2002f4c21c
陈序源 <chenjx0395@163.com>
2024-12-15 11:58:34 +0800

    运算符重载案例

--- 2024-12-14/Exercise/Exercise/Program.cs
+++ 2024-12-14/Exercise/Exercise/Program.cs
@@ -9,12 +9,78 @@ using System.Xml.Xsl;
 
 namespace Exercise
 {
+    class LimitedInt
+    {
+        const int MaxValue = 100;                        
+        const int MinValue = 0;
+
+        public static LimitedInt operator -(LimitedInt x)
+        {
+            //在这个奇怪的类中，取一个值的负数等于0
+            LimitedInt li = new LimitedInt();
+            li.TheValue = 0;
+            return li;
+        }
+
+        public static LimitedInt operator -(LimitedInt x, LimitedInt y)
+        {
+            LimitedInt li = new LimitedInt();
+            li.TheValue = x.TheValue - y.TheValue;
+            return li;
+        }
+
+        public static LimitedInt operator +(LimitedInt x, double y)
+        {
+            LimitedInt li = new LimitedInt();
+            li.TheValue = x.TheValue + (int)y;
+            return li;
+        }
+
+        private int _theValue = 0;
+
+        public int TheValue
+        {
+            get { return _theValue; }
+
+            set
+            {
+                if (value < MinValue)
+                    _theValue = 0;
+                else
+                    _theValue = value > MaxValue
+                        ? MaxValue
+                        : value;
+            }
+        }
+
+
+
+
+
+    }
+
+
+
     internal class Program
     {
 
         static void Main(string[] args)
         {
+            LimitedInt li1 = new LimitedInt();
+            LimitedInt li2 = new LimitedInt();
+            LimitedInt li3 = new LimitedInt();
 
+
+            li1.TheValue = 10; li2.TheValue = 26;
+
+
+            Console.WriteLine($" li1: {li1.TheValue}, li2: {li2.TheValue}");
+            li3 = -li1;
+            Console.WriteLine($"-{li1.TheValue} = {li3.TheValue}");
+            li3 = li2 - li1;
+            Console.WriteLine($" {li2.TheValue} - {li1.TheValue} = {li3.TheValue}");
+            li3 = li1 + 2;
+            Console.WriteLine($" {li1.TheValue} - {li2.TheValue} = {li3.TheValue}");
 
             // while 循环练习
             // Case1();
@@ -37,7 +103,7 @@ namespace Exercise
             // Case16();
             // Case17();
             // Case18();
-            Case19();
+            // Case19();
         }
 
         public static void Case19()
@@ -85,6 +151,7 @@ namespace Exercise
                 {
                     break;
                 }
+
             }
         }
 
